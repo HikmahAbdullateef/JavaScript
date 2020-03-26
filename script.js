2ef48713f00318e2ee5d62598c3b8eabd9a9fc35
Hikmah Abdullateef <Heekmera@gmail.com>
2020-03-26 20:37:28 +0000

    JS Task

---
+++ script.js
@@ -0,0 +1,18 @@
+let myName = ("Hikmah Abdullateef");
+
+console.log("My name is ${myNmae}.");
+
+let courses = ["HTML", "CSS", "JS"];
+
+console.log("The courses i registered for are ${courses[0]}, ${courses[1]}, ${courses[2]}");
+
+const numbers = []
+
+for (let i=1; i<=200; i++) {
+        if (i % 2 !==0) numbers.push(i);
+    }
+
+console.log(numbers);
+
+
+  
